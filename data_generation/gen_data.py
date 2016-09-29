4075a803502243315fae261e8fc74ff8bd029f5a
Sunny Golovine <sunny@golovinemail.org>
2016-09-28 21:55:57 -0400

    addeddata generation script

---
+++ data_generation/gen_data.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Sunny Golovine
+# sunny@golovinemail.org
+# golovine.org
+
+# This script will generate a data set of a set length
+# You can generate either worse case data (descending data set),
+# best case data set (ascending data set)
+# or an average case data set (random data set)
+
+# Each function takes a size argument and will generate a data set
+# with the given length
+
+from random import randint
+
+def best_case(size):
+    f = open('best_case.txt', 'w')
+    x = 0
+    while x < size:
+        f.write(str(x) + '\n')
+        x = x + 1
+    f.close()
+
+def avg_case(size):
+    f = open('avg_case.txt', 'w')
+    x = 0
+    while x < size:
+        f.write(str(randint(0,9999999)) + '\n')
+        x = x + 1
+    f.close()
+def worst_case(size):
+    f = open('worst_case.txt', 'w')
+    while size > 0:
+        f.write(str(size) + '\n')
+        size = size - 1
+    f.close()
+# Wrapper for all functions
+def gen(size):
+    best_case(size)
+    avg_case(size)
+    worst_case(size)
+
+
+if __name__ == "__main__":
+    gen(1000)
